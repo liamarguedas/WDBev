5556773f6c7ff6740ef24fbf35a4e39627f40d65
Liam <iliamftw2013@gmail.com>
2024-03-31 21:04:16 -0300

    add( ejs tags )

---
+++ tags/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  let seconds = new Date().getSeconds();
+
+  const data = {
+    title: "EJS Tags",
+    seconds: seconds,
+    items: ["apple", "banana", "cherry"],
+    htmlContent: "<strong>This is some strong text</strong>",
+  };
+  res.render("index.ejs", data);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
